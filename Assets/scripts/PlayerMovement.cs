6fee47741f595cd1222b88211eb4272633aa18d3
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-05-16 14:12:42 -0600

    chore: clean repo and enhance code

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -23,6 +23,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float maxPitch = 65f;
 
     private Rigidbody rb;
+    private PlayerInput playerInput;
+
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
 
     private Vector2 moveInput;
     private Vector2 lookInput;
@@ -36,9 +42,47 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
+        sprintAction = playerInput.actions["Sprint"];
+    }
+
+    private void OnEnable()
+    {
+        moveAction.performed += OnMovePerformed;
+        moveAction.canceled += OnMoveCanceled;
+
+        lookAction.performed += OnLookPerformed;
+        lookAction.canceled += OnLookCanceled;
+
+        jumpAction.performed += OnJumpPerformed;
+
+        sprintAction.performed += OnSprintPerformed;
+        sprintAction.canceled += OnSprintCanceled;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.performed -= OnMovePerformed;
+        moveAction.canceled -= OnMoveCanceled;
+
+        lookAction.performed -= OnLookPerformed;
+        lookAction.canceled -= OnLookCanceled;
+
+        jumpAction.performed -= OnJumpPerformed;
+
+        sprintAction.performed -= OnSprintPerformed;
+        sprintAction.canceled -= OnSprintCanceled;
+
+        moveInput = Vector2.zero;
+        lookInput = Vector2.zero;
+        isSprinting = false;
     }
 
     private void Update()
@@ -121,33 +165,40 @@ public class PlayerMovement : MonoBehaviour
         );
     }
 
-    public void OnMove(InputValue value)
+    private void OnMovePerformed(InputAction.CallbackContext context)
     {
-        moveInput = value.Get<Vector2>();
+        moveInput = context.ReadValue<Vector2>();
     }
 
-    public void OnLook(InputValue value)
+    private void OnMoveCanceled(InputAction.CallbackContext context)
     {
-        lookInput = value.Get<Vector2>();
+        moveInput = Vector2.zero;
     }
 
-    public void OnJump(InputValue value)
+    private void OnLookPerformed(InputAction.CallbackContext context)
     {
-        if (!value.isPressed) return;
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnLookCanceled(InputAction.CallbackContext context)
+    {
+        lookInput = Vector2.zero;
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
         if (!isGrounded) return;
 
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
-    public void OnSprint(InputValue value)
+    private void OnSprintPerformed(InputAction.CallbackContext context)
     {
-        isSprinting = value.Get<float>() > 0.5f;
+        isSprinting = true;
     }
 
-    private void OnDisable()
+    private void OnSprintCanceled(InputAction.CallbackContext context)
     {
-        moveInput = Vector2.zero;
-        lookInput = Vector2.zero;
         isSprinting = false;
     }
 }
